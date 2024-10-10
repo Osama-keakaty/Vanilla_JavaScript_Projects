a5c744ea4a6e576fd9b05e4d9a5809453a653ea6
Osama-keakaty <osama.k3ekaty2001@gmail.com>
2024-10-10 22:52:32 +0300

    remove initial values

--- Weather_App/main.js
+++ Weather_App/main.js
@@ -3,26 +3,26 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 const searchBox = document.querySelector('input');
 const searchButton = document.querySelector('button');
 const weather = document.querySelector(".weather");
+if (window.localStorage.getItem("city")){
+    weatherCheck(window.localStorage.getItem("city"));
+    weather.style.cssText = "display: flex";
+}
 async function weatherCheck(city) {
     try {
         const response = await fetch(apiUrl + `&q=${city}` + `&appid=${apiKey}`);
         if (response.ok) {
             let Data = await response.json();
-            console.log(Data);
-            console.log(Data.main);
-            console.log(Data.main.temp);
-            console.log(Data.name);
-            console.log(Data.weather.main);
             document.querySelector(".temp").innerHTML = `${Math.round(Data.main.temp)}°C`;
             document.querySelector(".city").innerHTML = Data.name;
             document.querySelector(".humidity").innerHTML = Data.main.humidity;
             document.querySelector(".weather-icon").src = `images/${Data.weather[0].main}.png`;
             document.querySelector(".wind").innerHTML = Data.wind.speed + "km/h";
+            localStorage.setItem("city",city);
         } else {
-            console.log("Error");
+           throw new Error("Connection Failed");
         }
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
 }
 searchButton.addEventListener("click", () => {
