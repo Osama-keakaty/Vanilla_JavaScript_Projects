a7f19a78bdecb8fbe321ae70b5c3e3e641e43105
Osama-keakaty <osama.k3ekaty2001@gmail.com>
2024-10-11 16:37:01 +0300

    To-Do list App

---
+++ To_Do_list_App/main.js
@@ -0,0 +1,47 @@
+const inputBox = document.getElementById("input-box");
+const taskButton= document.querySelector(".task-btn");
+const listContainer=document.querySelector(".list-container");
+
+
+// add event listener when button is clicked
+taskButton.addEventListener("click",()=>{
+
+    // check if the input bos is not empty
+    if (inputBox.value.trim()===""){
+        alert("Please enter your task");
+    } else {
+        let listItem = document.createElement("li");
+        let listItemContent=document.createTextNode(inputBox.value);
+        listItem.appendChild(listItemContent);
+        listContainer.appendChild(listItem);
+        let span = document.createElement('span');
+        span.innerHTML="\u00d7";
+        listItem.appendChild(span);
+        inputBox.value=" ";
+        saveData();
+    }  
+});
+
+
+// add event listener when the task or x symbol is clicked
+listContainer.addEventListener("click",(e)=>{
+    //check if the element that was clicked is li  
+    if (e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+        //check if the element that was clicked is span 
+    } else if (e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+
+// saving the tasks in local storage 
+function saveData(){
+    localStorage.setItem("tasks",listContainer.innerHTML);
+}
+// display the tasks in web bage
+function showData(){
+    listContainer.innerHTML=localStorage.getItem("tasks");
+}
+showData();
